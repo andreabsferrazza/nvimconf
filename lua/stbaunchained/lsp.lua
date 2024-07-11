9279f51f99cbafa39aa303ab1691debdd8f7856d
sba <andreabsferrazza@gmail.com>
2024-07-11 23:34:36 +0200

    added keymaps

--- lua/stbaunchained/lsp.lua
+++ lua/stbaunchained/lsp.lua
@@ -1,6 +1,5 @@
 local lsp = require('lsp-zero')
 lsp.preset('recommended')
-
 require("mason").setup()
 require("mason-lspconfig").setup({
 	ensure_installed = { 
@@ -14,10 +13,14 @@ require("mason-lspconfig").setup({
 		end,
 	},
 })
-
 lsp.on_attach( function(client,bufnr)
 	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+	vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.vws() end, opts)
+	vim.keymap.set('n', '<leader>vd', function() vim.lsp.buf.open_float() end, opts)
+	vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+	vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 end)
-
 lsp.setup()
