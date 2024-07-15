88a9d3d6948102c27e6e8f337a6eb5dbdd8f97d3
sba <andreabsferrazza@gmail.com>
2024-07-15 15:14:59 +0200

    Edited shortcuts

--- lua/stbaunchained/lsp.lua
+++ lua/stbaunchained/lsp.lua
@@ -11,7 +11,6 @@ require("mason-lspconfig").setup({
 lsp.on_attach( function(client,bufnr)
 	vim.keymap.set('n', '<leader>gd', function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.vws() end, opts)
 	vim.keymap.set('n', '<leader>vd', function() vim.lsp.buf.open_float() end, opts)
 	vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
