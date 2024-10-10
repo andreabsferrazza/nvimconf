82bf9719e8d29165000ff020e9ce3af1f4dfd66c
sba <andreabsferrazza@gmail.com>
2024-10-10 10:40:35 +0200

    Added resume and search from visual mode in live grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -9,3 +9,14 @@ vim.keymap.set("n", "<leader>fv", ":Telescope file_browser path=%:p:h select_buf
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ searc = vim.fn.input("Grep > ") })
 end)
+
+-- resume a previous live grep search
+vim.keymap.set("n", "<leader>gx", require("telescope.builtin").resume, {
+  noremap = true,
+  silent = true,
+  desc = "Resume",
+})
+
+-- in visual mode select a word and search it in the project with live grep 
+local default_opts = {noremap = true, silent = true}
+vim.api.nvim_set_keymap('v', '<leader>fg', 'y<ESC>:Telescope live_grep default_text=<c-r>0<CR>', default_opts)
